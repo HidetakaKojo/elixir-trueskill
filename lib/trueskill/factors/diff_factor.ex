14f18cc06ffba0cf51dc45eadc5f907c4aea03c2
Hidetaka Kojo <kojo@mixi.co.jp>
2015-11-12 02:43:53 +0900

    add reverse iterated loop

--- lib/trueskill/factors/diff_factor.ex
+++ lib/trueskill/factors/diff_factor.ex
@@ -2,16 +2,19 @@ defmodule Trueskill.Factors.DiffFactor do
   import Trueskill.Factors.SumFactorBase
   alias Trueskill.Gaussian.Distribution, as: Gaussian
 
-  def diff_teams(performances) do
+  def diff_teams(performances, is_reverse) do
     diffs = Range.new(0, (Enum.count(performances) - 2))
       |> Enum.to_list
       |> Enum.map(fn(_) ->
         %Trueskill.Variable{value: Gaussian.new, messages: %{diff: Gaussian.new, truncate: Gaussian.new}}
       end)
-    diff_teams(performances, diffs)
+    diff_teams(performances, diffs, is_reverse)
   end
-  def diff_teams(performances, diffs) do
-    coefficients = [1, -1]
+  def diff_teams(performances, diffs, is_reverse) do
+    coefficients = case is_reverse do
+      true  -> [-1, 1]
+      false -> [1, -1]
+    end
     length = Enum.count performances
     new_diffs = Range.new(0, length-2)
       |> Enum.to_list
@@ -28,35 +31,43 @@ defmodule Trueskill.Factors.DiffFactor do
     [new_diffs, delta]
   end
 
-  def update_team(diffs, team_performances) do
-    new_team_performances = update_team_recursive(diffs, team_performances)
+  def update_team(diffs, team_performances, is_reverse) do
+    new_team_performances = update_team_recursive(diffs, team_performances, is_reverse)
     delta = Enum.with_index(new_team_performances) |> Enum.map(fn({perf, idx}) ->
       Gaussian.subtract(perf.value, Enum.fetch!(team_performances, idx).value)
     end) |> Enum.max
     [new_team_performances, delta]
   end
-  def update_team_recursive([], [rest_team]) do
+  def update_team_recursive([], [rest_team], _is_reverse) do
     [rest_team]
   end
-  def update_team_recursive([first_diff|diffs], [first_team|teams]) do
+  def update_team_recursive([first_diff|diffs], [first_team|teams], is_reverse) do
     [second_team|rest_teams] = teams
 
     first_input_values = [first_diff, second_team] |> Enum.map(fn(x) -> x.value end)
     first_input_messages = [first_diff, second_team] |> Enum.map(fn(x) -> x.messages.diff end)
+    first_coefficients = case is_reverse do
+      true  -> [-1, 1]
+      false -> [1, 1]
+    end
     [new_first_team_value, new_first_team_message] =
-      sum(first_team.value, first_team.messages.diff, first_input_values, first_input_messages, [1, 1])
+      sum(first_team.value, first_team.messages.diff, first_input_values, first_input_messages, first_coefficients)
     new_first_team =%Trueskill.Variable{
       value: new_first_team_value,
       messages: Map.merge(first_team.messages, %{diff: new_first_team_message})}
 
     second_input_values = [first_team, first_diff] |> Enum.map(fn(x) -> x.value end)
     second_input_messages = [first_team, first_diff] |> Enum.map(fn(x) -> x.messages.diff end)
+    second_coefficients = case is_reverse do
+      true  -> [1, 1]
+      false -> [1, -1]
+    end
     [new_second_team_value, new_second_team_message] =
-      sum(second_team.value, second_team.messages.diff, second_input_values, second_input_messages, [1, -1])
+      sum(second_team.value, second_team.messages.diff, second_input_values, second_input_messages, second_coefficients)
     new_second_team =%Trueskill.Variable{
       value: new_second_team_value,
       messages: Map.merge(second_team.messages, %{diff: new_second_team_message})}
 
-    [new_first_team] ++ update_team_recursive(diffs, [new_second_team|rest_teams])
+    [new_first_team] ++ update_team_recursive(diffs, [new_second_team|rest_teams], is_reverse)
   end
 end
--- lib/trueskill/factors/iterated_factor.ex
+++ lib/trueskill/factors/iterated_factor.ex
@@ -14,10 +14,12 @@ defmodule Trueskill.Factors.IteratedFactor do
   end
 
   defp compare_two_teams(team_performances, ranks, draw_margin, options) do
-      [team_performance_diffs, _diff_delta] = Trueskill.Factors.DiffFactor.diff_teams(team_performances)
-      [truncated_diffs, _truncate_delta] = Trueskill.Factors.TruncateFactor.update(team_performance_diffs, ranks, draw_margin)
-      [new_team_performances, _update_delta] = Trueskill.Factors.DiffFactor.update_team(truncated_diffs, team_performances)
-      Trueskill.Variable.merge_messages(:sum, new_team_performances, team_performances)
+    IO.inspect team_performances
+    [team_performance_diffs, _diff_delta] = Trueskill.Factors.DiffFactor.diff_teams(team_performances, false)
+    [truncated_diffs, _truncate_delta] = Trueskill.Factors.TruncateFactor.update(team_performance_diffs, ranks, draw_margin)
+    [new_team_performances, _update_delta] = Trueskill.Factors.DiffFactor.update_team(truncated_diffs, team_performances, false)
+    IO.inspect new_team_performances
+    Trueskill.Variable.merge_messages(:sum, new_team_performances, team_performances)
   end
 
   defp compare_multi_teams(team_performances, ranks, draw_margin, options) do
@@ -28,9 +30,14 @@ defmodule Trueskill.Factors.IteratedFactor do
   end
   defp compare_multi_teams(team_performances, ranks, draw_margin, options, num) do
     max_delta = options[:max_delta] || @max_delta
-    [team_performance_diffs, diff_delta] = Trueskill.Factors.DiffFactor.diff_teams(team_performances)
+    [team_performance_diffs, diff_delta] = Trueskill.Factors.DiffFactor.diff_teams(team_performances, false)
     [truncated_diffs, truncate_delta] = Trueskill.Factors.TruncateFactor.update(team_performance_diffs, ranks, draw_margin)
-    [new_team_performances, update_delta] = Trueskill.Factors.DiffFactor.update_team(truncated_diffs, team_performances)
-    compare_multi_teams(new_team_performances, ranks, draw_margin, options, num - 1)
+    [new_team_performances, update_delta] = Trueskill.Factors.DiffFactor.update_team(truncated_diffs, team_performances, false)
+
+    [reversed_team_performance_diffs, diff_delta] = Trueskill.Factors.DiffFactor.diff_teams(Enum.reverse(new_team_performances), true)
+    [reversed_truncated_diffs, truncate_delta] = Trueskill.Factors.TruncateFactor.update(reversed_team_performance_diffs, Enum.reverse(ranks), draw_margin)
+    [reversed_new_team_performances, update_delta] = Trueskill.Factors.DiffFactor.update_team(reversed_truncated_diffs, Enum.reverse(team_performances), true)
+
+    compare_multi_teams(Enum.reverse(reversed_new_team_performances), ranks, draw_margin, options, num - 1)
   end
 end
