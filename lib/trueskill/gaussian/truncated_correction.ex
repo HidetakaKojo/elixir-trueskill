aeed22e856ab98277e2130af80910eac8b7729a6
Hidetaka Kojo <kojo@mixi.co.jp>
2015-11-11 22:33:51 +0900

    use Statistics.pdf

--- lib/trueskill/gaussian/truncated_correction.ex
+++ lib/trueskill/gaussian/truncated_correction.ex
@@ -2,58 +2,46 @@ defmodule Trueskill.Gaussian.TruncatedCorrection do
   alias Trueskill.Gaussian.Distribution, as: Gaussian
   alias Statistics.Distributions.Normal, as: Statistics
 
-  def w_exceeds_margin(diff, epsilon) do
-    if Statistics.cdf(diff - epsilon) < 2.2e-162 do
-      if((diff < 0.0),  do: 1.0, else: 0.0)
-    else
-      v = v_exceeds_margin(diff, epsilon)
-      v * (v + diff - epsilon)
-    end
-  end
-
-  def w_within_margin(diff, epsilon) do
-    abs_diff = abs(diff)
-    denom = Statistics.cdf(epsilon - abs_diff) - Statistics.cdf(-epsilon - abs_diff)
-    if denom < 2.2e-162 do
-      1.0
-    else
-      vt = v_within_margin(abs_diff, epsilon)
-      :math.pow(vt, 2) + (
-        (epsilon - abs_diff) *
-        Gaussian.value_at(Gaussian.new(0, 1.0), (epsilon - abs_diff)) -
-        (-epsilon - abs_diff) *
-        Gaussian.value_at(Gaussian.new(0, 1.0), (-epsilon - abs_diff))
-      ) / denom
-    end
-  end
-
   def v_exceeds_margin(diff, epsilon) do
     denom = Statistics.cdf(diff - epsilon)
-    if (denom < 2.2e-162) do
+    if denom == 0.0 do
       -diff + epsilon
     else
       Gaussian.value_at(Gaussian.new(0, 1.0), (diff - epsilon)) / denom
     end
   end
 
+  def w_exceeds_margin(diff, epsilon) do
+    v = v_exceeds_margin(diff, epsilon)
+    v * (v + diff - epsilon)
+  end
+
   def v_within_margin(diff, epsilon) do
     abs_diff = abs(diff)
     denom = Statistics.cdf(epsilon - abs_diff) - Statistics.cdf(-epsilon - abs_diff)
-    if denom < 2.2e-162 do
-      if diff < 0 do
-        - diff - epsilon
-      else
-        - diff + epsilon
-      end
+    numer = Statistics.pdf(-epsilon - abs_diff) - Statistics.pdf(epsilon - abs_diff)
+    v = if denom == 0.0 do
+      epsilon - abs_diff
     else
-      num = Gaussian.value_at(Gaussian.new(0, 1.0), (-epsilon - abs_diff)) -
-        Gaussian.value_at(Gaussian.new(0, 1.0), (epsilon - abs_diff))
-      if diff < 0 do
-        - num / denom
-      else
-        num / denom
-      end
+      numer / denom
     end
+    if diff < 0 do
+      - v
+    else
+      v
+    end
+  end
+
+  def w_within_margin(diff, epsilon) do
+    abs_diff = abs(diff)
+    denom = Statistics.cdf(epsilon - abs_diff) - Statistics.cdf(-epsilon - abs_diff)
+      vt = v_within_margin(abs_diff, epsilon)
+    :math.pow(vt, 2) + (
+      (epsilon - abs_diff) *
+      Statistics.pdf(epsilon - abs_diff) -
+      (-epsilon - abs_diff) *
+      Statistics.pdf(-epsilon - abs_diff)
+    ) / denom
   end
 
 end
